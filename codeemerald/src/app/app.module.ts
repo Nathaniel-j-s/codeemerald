403337906105cfce5e4f2def5c736adb79fc440e
Nathaniel-j-s <nathaniel.j.mith@gmail.com>
2017-07-09 23:30:01 -0600

    templating out some html

--- codeemerald/src/app/app.module.ts
+++ codeemerald/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { AccountComponent } from './components/account/account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule
